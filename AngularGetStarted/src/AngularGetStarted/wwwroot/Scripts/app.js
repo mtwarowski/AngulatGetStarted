045885b9d8484d10d9900ee3bb957eb1902c7a6d
serekmt <serkemt@wp.pl>
2015-10-04 11:46:17 +0200

    First look. Controller view + http request

---
+++ AngularGetStarted/src/AngularGetStarted/wwwroot/Scripts/app.js
@@ -0,0 +1,43 @@
+﻿//(function () {
+//     angular.module("mainApp", [])
+//     .controller('mainController', function ($scope, $http) {
+
+//         var onUserComplete = function (responce) {
+//             $scope.user = responce.data;
+//         };
+
+//         var onError = function (reson) {
+//             $scope.error = "Could not fetch the user.";
+//         }
+
+//         $http.get("https://api.github.com/users/mtwarowski")
+//         .then(onUserComplete, onError);
+
+//         $scope.message = "Hello from Ctrl";
+//     });
+
+//}());
+
+(function () {
+    var app = angular.module("mainApp", []);
+    var mainCtrl = function ($scope, $http) {
+
+        var onUserComplete = function (responce) {
+            $scope.user = responce.data;
+        };
+
+        var onError = function (reson) {
+            $scope.error = "Could not fetch the user.";
+        }
+
+        $http.get("https://api.github.com/users/mtwarowski")
+        .then(onUserComplete, onError);
+
+        $scope.message = "Hello from Ctrl";
+    };
+
+
+    //protection from minifier
+    app.controller('mainController', ["$scope", "$http", mainCtrl]);
+
+}());
