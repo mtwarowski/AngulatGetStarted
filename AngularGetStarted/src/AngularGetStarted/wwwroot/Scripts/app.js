11dcfd6aadafcf399fb46ec82f44027aa3513320
serekmt <serkemt@wp.pl>
2015-10-04 20:34:23 +0200

    Ng-Repeate filters and orderBy

--- AngularGetStarted/src/AngularGetStarted/wwwroot/Scripts/app.js
+++ AngularGetStarted/src/AngularGetStarted/wwwroot/Scripts/app.js
@@ -24,16 +24,29 @@
 
         var onUserComplete = function (responce) {
             $scope.user = responce.data;
+            
+            $http.get($scope.user.repos_url)
+                .then(onRepos, onError);
+        };
+
+        var onRepos = function (responce) {
+            $scope.repos = responce.data;
         };
 
         var onError = function (reson) {
-            $scope.error = "Could not fetch the user.";
+            $scope.error = "Could not fetch the data.";
         }
 
-        $http.get("https://api.github.com/users/mtwarowski")
-        .then(onUserComplete, onError);
 
-        $scope.message = "Hello from Ctrl";
+        $scope.search = function (userName) {
+            $http.get("https://api.github.com/users/" + userName)
+            .then(onUserComplete, onError);
+
+        };
+
+        $scope.userName = "mtwarowski";
+        $scope.message = "GitHub Viewer";
+        $scope.repoSortOrder = "+id";
     };
 
 
